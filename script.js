ba603ea488e10ee82fbfd587066fb143f6914ed4
Jarrett <contact@jarrettperkins.me>
2022-10-28 07:42:02 -0500

    Completed getPlayerChoice function

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 function numberToString(play) {
     switch (true) {
-        case play == 1:
+        case play == 0x01:
             return 'Rock'
-        case play == 2:
+        case play == 0x02:
             return 'Paper'
-        case play == 3:
+        case play == 0x03:
             return 'Scissors'
     }
 }
 
-function getPlayerChoice() {
-    
+function getPlayerChoice(choice) {
+    choice = choice.toLowerCase()
+
+    switch(true) {
+        case choice == 'rock':
+            return 0x10
+        case choice == 'paper':
+            return 0x20
+        case choice == 'scissors':
+            return 0x30
+    }
 }
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1
+    return Math.floor(Math.random() * 3).toString(16)
 }
 
 function rockPaperScissors(playerSelection, computerSelection) {
     playerSelection
 }
 
-const playerSelection = 'rock'
+let getPlayerSelection = prompt("Rock, paper, or scissors?")
+
+const playerSelection = getPlayerChoice(getPlayerSelection)
 const computerSelection = getComputerChoice()
